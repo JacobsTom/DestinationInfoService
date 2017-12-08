fe1ad2284c6792c2776f4331cad3f0b30d78d3d6
Tom Jacobs <Tom@192.168.0.17>
2017-12-08 12:17:36 +0100

    docker hub stuff

---
+++ src/main/docker/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "********************************************************"
+
+echo "Starting DestinationInfoService Server"
+
+echo "********************************************************"
+
+java -jar /usr/local/DestinationInfoService/@project.build.finalName@.jar
