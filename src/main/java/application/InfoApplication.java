8ae7e5758b35fcb2255d74733c9d76bf431d83d3
Tom Jacobs <Tom@192.168.0.17>
2017-12-08 12:31:44 +0100

    even more docker hub stuff

--- src/main/java/application/InfoApplication.java
+++ src/main/java/application/InfoApplication.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(basePackages = { "controller"})
 public class InfoApplication {
@@ -22,16 +22,16 @@ public class InfoApplication {
         SpringApplication.run(InfoApplication.class, args);
     }
 }
-
-@RestController
-class ServiceInstanceRestController {
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
-            @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
-    }
-}
+//
+//@RestController
+//class ServiceInstanceRestController {
+//
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
+//
+//    @RequestMapping("/service-instances/{applicationName}")
+//    public List<ServiceInstance> serviceInstancesByApplicationName(
+//            @PathVariable String applicationName) {
+//        return this.discoveryClient.getInstances(applicationName);
+//    }
+//}
